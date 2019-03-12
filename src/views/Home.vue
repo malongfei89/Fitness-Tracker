8d5f62eda22460d1de688264851092e4e5aa16bd
Longfei Ma <mg071121mg@gmail.com>
2019-03-12 11:29:07 -0400

    3/12/19

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+    <Welcome />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Welcome from '@/components/Welcome.vue'
+import Header from '@/components/Header.vue'
+export default {
+  name: 'home',
+  components: {
+    Welcome,
+    Header
+  }
+}
+</script>
