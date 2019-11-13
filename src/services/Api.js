4f3600cff9e9742ce515369e6a0cda018b047179
malongfei89 <39601454+malongfei89@users.noreply.github.com>
2019-11-12 19:59:03 -0500

    Update Api.js

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://ec2-3-134-94-92.us-east-2.compute.amazonaws.com:3000'
+    baseURL: 'https://ec2-3-134-94-92.us-east-2.compute.amazonaws.com:3000'
   })
 }
